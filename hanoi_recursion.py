4799d12becdf5578b7db7f0323e8ef1773f16dc1
Ming Xu <xm19930629@163.com>
2021-04-16 10:12:47 +0800

    hanoi recursion

---
+++ hanoi_recursion.py
@@ -0,0 +1,21 @@
+
+
+
+def hanoi(n, a, b, c):
+    """
+    汉诺塔递归算法
+    :param n: n 个盘子
+    :param a: Ａ 柱子
+    :param b: B 柱子
+    :param c: C 柱子
+    :return: ｎ 个盘子从 A-C
+    """
+    if n > 0:
+        hanoi(n-1, a, c, b)
+        print("move from %s to %s" % (a, c))
+        hanoi(n-1, b, a, c)
+
+
+
+if __name__ == "__main__":
+    hanoi(20, 'A', 'B', 'C')
